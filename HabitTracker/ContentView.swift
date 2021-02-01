4da27591f86b5c9ab529a12f2fbbd078f6419328
Joep Hinderink <hello@joephinderink.nl>
2021-02-01 11:22:12 +0100

    Commit

--- HabitTracker/ContentView.swift
+++ HabitTracker/ContentView.swift
@@ -7,10 +7,46 @@
 
 import SwiftUI
 
+// Design from: https://dribbble.com/shots/11109255-Habit-tracking-App/attachments/2709082?mode=media
+
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color("backgroundcolor")
+                .ignoresSafeArea()
+            VStack {
+                VStack {
+                    HStack {
+                        Image(systemName: "line.horizontal.3")
+                            .resizable()
+                            .frame(width: 20, height: 15, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .padding()
+                        Spacer()
+                        Text("Habits")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .padding(.trailing, UIScreen.main.bounds.width * 0.43)
+                    }
+                    .foregroundColor(.white)
+                    ForEach(0 ..< 3) { item in
+                       HabitCell(title: "Meditation")
+                        .padding(8)
+                    }
+                    HStack {
+                        Image(systemName: "plus.square")
+                            .resizable()
+                            .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(Color("purple"))
+                        Text("Add a new habit")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("purple"))
+                    }
+                    .padding()
+                }
+                Spacer()
+            }
+        }
     }
 }
 
