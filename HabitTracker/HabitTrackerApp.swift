c5fe0e109f64d6b207b9956317374183bbc1b354
Joep Hinderink <hello@joephinderink.nl>
2021-02-01 09:55:14 +0100

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Joep Hinderink on 01/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
